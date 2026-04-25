079e42514c6b45e0d6badaffbdc3dad401198641
nicolascastilla12 <tu-email@example.com>
2026-04-24 19:49:00 -0500

    Commit inicial - Proyecto React

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,10 @@
+function ProductCard() {
+  return (
+    <section>
+      <h3>Producto 1</h3>
+      <p>  $ 10.00</p>
+    </section>
+   ) 
+}
+
+export default ProductCard
---
+++ src/components/navbar.jsx
@@ -0,0 +1,15 @@
+function App() {
+    return(
+    <>    
+        <NavBar  />
+            <h1> bienvenidos </h1>
+
+         <section>
+            <h1> bienvenido nicolas castilla</h1>
+            <h2>  programacion para la web </h2>
+            <h3> CESDE Bogotá </h3>
+         </section>
+    </>
+    )
+}
+export default App  
